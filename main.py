538d036eb96a505b58f1c3d627c5913311317368
mine9607 <81383389+mine9607@users.noreply.github.com>
2024-12-24 13:19:05 -0500

    Added game clock to control framerate

--- main.py
+++ main.py
@@ -11,15 +11,26 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    # Create a game clock to control framerate 
+    game_clock = pygame.time.Clock()
+    dt = 0
+
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
+                pygame.quit() # cleanly shut down pygame
                 return
+        
+        # Update game logic
 
 
+        # Render
         screen.fill("black")
-
         pygame.display.flip()
+
+        # Cap the frame rate and calculate delta time
+        dt = game_clock.tick(60)/1000
 
 if __name__ == "__main__":
    main()
